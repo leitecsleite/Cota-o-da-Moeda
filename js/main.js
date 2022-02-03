64e6408bfd701bb0a04fbbdc98e61baddeb9f21c
Lucas <carvalho1104@hotmail.com>
2022-02-03 16:36:48 -0300

    time

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
 const url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'
 
-setInterval(() =>{  
-fetch(url)
-.then(resolv => resolv.json())
-.then(result =>{
-    const data = result 
-
-    const {BTCBRL,EURBRL,USDBRL } = data
-    const{bid} = BTCBRL 
-     let dataMoeda = [] 
-     dataMoeda = [BTCBRL,EURBRL, USDBRL]
-
-     const valueMoeda = dataMoeda.map(moeda => {
-           const moedinha = moeda.bid
-           return moedinha
-     })
-     
-        /*console.log(dataMoeda)
-        console.log(valueMoeda)*/ 
-    
-    buyDolar.innerHTML = valueMoeda[2]
-    buyEuro.innerHTML = valueMoeda[1]
-    buyBitcoin.innerHTML = valueMoeda[0]
-
-})
-},1000)
+/* moeda */
 
 setInterval(() => {
-     const data = new Date(); 
-     
-     const hora = data.getHours();
-     const min = data.getMinutes();
-     const seg = data.getSeconds(); 
+    fetch(url)
+        .then(resolv => resolv.json())
+        .then(result => {
+            const data = result
 
-    /* console.log(hora,min, seg)*/
+            const { BTCBRL, EURBRL, USDBRL } = data
+            const { bid } = BTCBRL
+            let dataMoeda = []
+            dataMoeda = [BTCBRL, EURBRL, USDBRL]
 
-     hours.innerHTML = hora; 
-     minute.innerHTML =min; 
-     seconds.innerHTML =seg;
+            const valueMoeda = dataMoeda.map(moeda => {
+                const moedinha = moeda.bid
+                return moedinha
+            })
 
-},1000)
+            /*console.log(dataMoeda)
+            console.log(valueMoeda)*/
+
+            buyDolar.innerHTML = valueMoeda[2]
+            buyEuro.innerHTML = valueMoeda[1]
+            buyBitcoin.innerHTML = valueMoeda[0]
+
+        })
+}, 1000)
+
+/* Relogio */
+
+setInterval(() => {
+    const data = new Date();
+
+    const hora = data.getHours();
+    const min = data.getMinutes();
+     let seg = data.getSeconds();
+
+
+    const valueSeconds = parseInt(seconds.textContent);
+    const valueMinutes = parseInt(minute.textContent);
+    const valueHour = parseInt(hours.textContent)
+ 
+    if (valueSeconds < 9 || valueSeconds == 59) {
+        seconds.innerHTML = `0${seg}`;
+        
+    } else {
+        seconds.innerHTML = seg 
+    }
+
+    if (valueMinutes < 9 || valueMinutes == 56) {
+        minute.innerHTML = `0${min}`;
+    } else {
+        minute.innerHTML = min;
+    }
+
+    if (valueHour < 9||  valueHour == 23) {
+        hours.innerHTML = `0${hora}`;
+    } else {
+        hours.innerHTML = hora;
+    }
+
+}, 1000)
